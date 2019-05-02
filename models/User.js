8d1b30312214044cd64bf45e1a042fe24b241fc0
camimaya21 <camilamaya21@gmail.com>
2019-05-02 23:58:53 +0200

    change confirm type in user's model

--- models/User.js
+++ models/User.js
@@ -5,11 +5,12 @@ const userSchema = new Schema({
   username: {type: String, required: true},
   password: {type: String, required: true},
   role: {type: String, enum: ['guest', 'admin'], default:'guest', required: true},
-  confirm: {type: Boolean, default: false},
+  confirm: {type: String, enum: ['yes', 'no'], default: 'no'},
   name: {type: String },
   numberPeople: {type: Number, default: 1},
   alergies: {type: String, default:'no'},
-  specialDiet: {type: String, enum: ['no', 'vegan', 'vegetarian', 'celiaco', 'paleo']}
+  specialDiet: {type: String, enum: ['no', 'vegan', 'vegetarian', 'celiaco', 'paleo']},
+  coments: {type: String}
 }, {
   timestamps: {
     createdAt: 'created_at',
