0d37e15d3ed0ba4e56f558263c29bf970046ba6e
camimaya21 <camilamaya21@gmail.com>
2019-05-08 20:01:38 +0200

    login errorHandle

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import AuthAPI from "../utils/auth";
+import classNames from "classnames"
 
 import { USER_UPDATED } from "../actions";
 import "./loginForm.css";
@@ -9,29 +10,37 @@ class LoginForm extends Component {
   constructor() {
     super();
     this.state = {
-      username: "",
-      password: "",
-      error: null
-    };
+      username: '',
+      password: '',
+      missingCredentials: null,
+      wrongCredentials: null
+    }
   }
 
   handleLogin() {
     const { username, password } = this.state;
     const { updateUser, history, isLogged } = this.props;
 
-    AuthAPI.login(username, password)
-      .then(user => {
-        updateUser(user);
-        isLogged();
-        history.push("/");
-      })
-      .catch(e => {
-        this.setState({ error: e });
-      });
+    username && password ? (
+
+      AuthAPI.login(username.length, password.length)
+        .then(user => {
+          updateUser(user);
+          isLogged();
+          history.push("/");
+        })
+        .catch(e => {
+          this.setState({ wrongCredentials: true });
+        })
+    ) : (
+    this.setState({missingCredentials: true })
+    )
   }
 
   render() {
-    const { username } = this.state;
+    const { username, missingCredentials, wrongCredentials } = this.state;
+    const css = classNames('login-input', {'login-error': missingCredentials || wrongCredentials })
+
     return (
       <div className="login-page">
         <div className="login-wrapper">
@@ -47,23 +56,26 @@ class LoginForm extends Component {
           </h3>
 
           <input
-            className="login-input"
+            className={css}
             placeholder="Pon tu usuario aquí..."
             value={username}
-            onChange={e => this.setState({ username: e.target.value })}
+            onChange={e => this.setState({ username: e.target.value, missingCredentials: false, wrongCredentials: false
+            })}
           />
 
           <input
-            className="login-input"
+            className={css}
             placeholder="Buena suerte con la contraseña..."
             type="password"
-            onChange={e => this.setState({ password: e.target.value })}
+            onChange={e => this.setState({ password: e.target.value, missingCredentials: false, wrongCredentials: false })}
           />
+          {missingCredentials && <p className="login-error-text">Ups, falta el usuario o la contraseña</p>}
+          {wrongCredentials && <p className="login-error-text">Vaya... contraseña incorrecta</p>}
 
           <p className="login-details">
             Si te atascas con la contraseña, llámanos <span role="img" aria-label="wink">😉</span>
           </p>
-          <div className="btn-wrapper" style={{justifyContent: 'flex-start'}}>
+          <div className="btn-wrapper disable" style={{justifyContent: 'flex-start'}}>
             <button style={{width: '100%'}} onClick={() => this.handleLogin()}>> Empezar</button>
           </div>
         </div>
